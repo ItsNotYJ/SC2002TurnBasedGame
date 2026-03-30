c659a5c882d27c012f40330924cf0ec56984d18e
Harini <jonnadaharini9@gmail.com>
2026-03-30 18:14:44 +0800

    added in all item classes, all status effect classes, and ActionItemUse. Also edited IStatusEffect to include decreaseDuration method

--- src/EffectStun.java
+++ src/EffectStun.java
@@ -1,24 +1,31 @@
 public class EffectStun implements IStatusEffect {
-
-    public EffectStun() { }
+    private int duration = 2;
 
     @Override
     public void applyEffect(Combatant target) {
-
+        // force the target to skip their turn
+        target.setSkipTurn(true);
+        System.out.println(target.getCombatantName() + " is stunned and cannot move!");
     }
 
     @Override
     public void removeEffect(Combatant target) {
-
+        // allow the target to move again once the stun wears off
+        target.setSkipTurn(false);
+        System.out.println(target.getCombatantName() + " is no longer stunned.");
+    }
+    @Override
+    public void decreaseDuration() {
+        duration--;
     }
 
     @Override
     public int getEffectDuration() {
-        return 2;
+        return duration;
     }
 
     @Override
     public boolean isEffectExpired() {
-        return false;
+        return duration <= 0;
     }
 }
--- src/ItemSmokeBomb.java
+++ src/ItemSmokeBomb.java
@@ -5,6 +5,13 @@ public class ItemSmokeBomb extends Item {
     // TODO: Add code
     @Override
     public void useItem(Combatant user, Combatant target, BattleEngine engine) {
+        // create the smoke effect
+        EffectSmokeBomb smoke = new EffectSmokeBomb();
 
+        // apply the effect to player who used the item
+        user.setStatusEffect(smoke);
+
+        // notify console
+        System.out.println(user.getCombatantName() + " threw a Smoke Bomb!");
     }
 }
