2add3b8903b55f3d49548ec602e7de1b78410715
Ho Yan Jie <yanjieho@gmail.com>
2026-04-07 15:46:41 +0800

    Fix EffectDefend bug where there were multiple Defend Effect instances

--- src/EffectDefend.java
+++ src/EffectDefend.java
@@ -1,17 +1,28 @@
 public class EffectDefend implements IStatusEffect {
+    private final String effectName = "Defend";
     private int duration = 2;
+
+    @Override
+    public String getEffectName() {
+        return effectName;
+    }
 
     @Override
     public void applyEffect(Combatant target) {
         for (IStatusEffect e : target.getActiveEffects()) {
-            if (e instanceof EffectDefend) {
-                System.out.println("You already have a defend status active!");
+            if (e instanceof EffectDefend effect && e != this) {
+                // Our team decided to refresh the duration of the defend effect rather than wasting the player's action
+                effect.refreshDuration();
+                System.out.println("\nYour defensive stance is refreshed! Duration reset to 2 turns.");
+                
+                // remove the old defend effect and replace as it is replaced with the new effect
+                target.getActiveEffects().remove(this);
                 return;
             }
         }
 
         target.setDefense(target.getDefense() + 10);
-        System.out.println(target.getCombatantName() + " braces for impact! Defense increased by 10.");
+        System.out.println("\n" + target.getCombatantName() + " braces for impact! Defense increased by 10.");
     }
 
     @Override
@@ -31,5 +42,9 @@ public class EffectDefend implements IStatusEffect {
     @Override
     public boolean isEffectExpired() {
         return duration <= 0;
+    }
+
+    public void refreshDuration() {
+        duration = 2;
     }
 }
